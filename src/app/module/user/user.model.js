857121321b8adacf496a7763dc02069639209d02
Fahad <205fahadmohammad@gmail.com>
2023-11-17 01:57:05 +0600

    user model done

---
+++ src/app/module/user/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose
+
+const UserSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    domain: {
+        type: String,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: true
+    },
+})
+
+export const User = model('User', UserSchema)
+
